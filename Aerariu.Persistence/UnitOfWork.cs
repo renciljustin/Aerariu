fbdda7d0a0117126a1cf80e0df6dc613f4919570
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-16 04:24:05 +0800

    Implement Unit-of-Work pattern.

---
+++ Aerariu.Persistence/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Aerariu.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerariu.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AerariuDbContext _dbContext;
+        public UnitOfWork(AerariuDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+
+        public void Commit()
+            => _dbContext.SaveChanges();
+
+        public async Task CommitAsync()
+            => await _dbContext.SaveChangesAsync();
+
+        public void Rollback()
+            => _dbContext.Dispose();
+
+        public async Task RollbackAsync()
+            => await _dbContext.DisposeAsync();
+    }
+}
