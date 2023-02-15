016bc5c70c7ff6f8b27fddaa569255c75b80b07b
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-16 04:08:56 +0800

    Configure database context.

--- Aerariu.API/Program.cs
+++ Aerariu.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Aerariu.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 namespace Aerariu.API
 {
     public class Program
@@ -13,6 +16,8 @@ namespace Aerariu.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            ConfigureDbContext(builder);
 
             var app = builder.Build();
 
@@ -31,6 +36,14 @@ namespace Aerariu.API
             app.MapControllers();
 
             app.Run();
+        }
+
+        private static void ConfigureDbContext(WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<AerariuDbContext>(options =>
+                            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            //builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
     }
 }
---
+++ Aerariu.Persistence/AerariuDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Aerariu.Core.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aerariu.Persistence
+{
+    public class AerariuDbContext : DbContext
+    {
+        public AerariuDbContext(DbContextOptions<AerariuDbContext> options): base(options) { }
+    }
+}
