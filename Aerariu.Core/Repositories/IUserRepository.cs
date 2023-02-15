6a0c35f129b785dc3bbbc1f5057a7e64a8245e08
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-16 04:53:54 +0800

    Add User repository.

---
+++ Aerariu.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Aerariu.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerariu.Core.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<bool> CheckPasswordAsync(User user, string password);
+        Task CreateUserAsync(User user, string password);
+        Task<IEnumerable<string>> GetRolesAsync(User user);
+    }
+}
---
+++ Aerariu.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using Aerariu.Core.Models;
+using Aerariu.Core.Repositories;
+using Aerariu.Utils.Tools;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aerariu.Persistence.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(AerariuDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<bool> CheckPasswordAsync(User user, string password)
+        {
+            var passwordHash = user.PasswordHash;
+            var passwordSalt = user.PasswordSalt;
+
+            var passwordMatched = await PasswordHasher.VerifyPasswordAsync(password, passwordHash, passwordSalt);
+
+            return passwordMatched;
+        }
+
+        public async Task CreateUserAsync(User user, string password)
+        {
+            (string passwordHash, string passwordSalt) = await PasswordHasher.HashPasswordAsync(password);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await base.AddAsync(user);
+        }
+
+        public async Task<IEnumerable<string>> GetRolesAsync(User user)
+        {
+            var userId = user.Id;
+
+            return await _entitySet.Include(user => user.UserRoles)
+                .ThenInclude(userRole => userRole.Role)
+                .Where(user => user.Id == userId)
+                .SelectMany(user => user.UserRoles.Select(role => role.Role.RoleName))
+                .ToListAsync();
+        }
+    }
+}
