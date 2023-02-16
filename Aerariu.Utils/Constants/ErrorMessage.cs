f31c7bdfd8eac8c3eb938402a960747af7bc1912
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-17 03:32:40 +0800

    Update error message constants.

--- Aerariu.Utils/Constants/ErrorMessage.cs
+++ Aerariu.Utils/Constants/ErrorMessage.cs
@@ -10,7 +10,7 @@ namespace Aerariu.Utils.Constants
     {
         public const string User_DuplicateUsername = "Username is already used.";
         public const string User_InvalidCredentials = "Invalid username or password.";
-        public const string User_NotFound = "Cannot find user.";
+        public const string User_NotFound = "Cannot find the user.";
 
         public const string RefreshToken_Expired = "The refresh token is expired.";
         public const string RefreshToken_Invalid = "The refresh token is invalid.";
