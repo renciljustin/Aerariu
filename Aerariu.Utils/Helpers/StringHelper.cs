a13897b5ee0675d73ad5d092f537d212c5d84cde
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-17 02:10:40 +0800

    Add StringHelper class.
    Add new method GenerateBase64String, GenerateBase64StringAsync.

---
+++ Aerariu.Utils/Helpers/StringHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerariu.Utils.Helpers
+{
+    public static class StringHelper
+    {
+        public static string GenerateBase64String()
+        {
+            var randomNumber = new byte[32];
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
+        }
+
+        public static async Task<string> GenerateBase64StringAsync()
+            => await Task.Run(() => GenerateBase64String());
+        
+    }
+}
