4ef9929e63cc4947132d31f79e897a3f0d9df19c
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-17 02:10:53 +0800

    Add Refresh Token repository.

---
+++ Aerariu.Core/Repositories/IRefreshTokenRepository.cs
@@ -0,0 +1,16 @@
+﻿using Aerariu.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerariu.Core.Repositories
+{
+    public interface IRefreshTokenRepository : IRepository<RefreshToken>
+    {
+        Task<RefreshToken> CreateTokenAsync(Guid userId);
+        void Refresh(RefreshToken token);
+        void Revoke(RefreshToken token);
+    }
+}
---
+++ Aerariu.Persistence/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,44 @@
+﻿using Aerariu.Core.Models;
+using Aerariu.Core.Repositories;
+using Aerariu.Utils.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerariu.Persistence.Repositories
+{
+    public class RefreshTokenRepository : Repository<RefreshToken>, IRefreshTokenRepository
+    {
+        public RefreshTokenRepository(AerariuDbContext dbContext) : base(dbContext) { }
+
+        public async Task<RefreshToken> CreateTokenAsync(Guid userId)
+        {
+            var token = await StringHelper.GenerateBase64StringAsync();
+
+            var refreshToken = new RefreshToken
+            {
+                Id = Guid.NewGuid(),
+                UserId = userId,
+                Token = token,
+                ValidTo = DateTime.UtcNow.AddHours(24),
+                TotalRefresh = 0,
+                IsRevoked = false
+            };
+
+            return refreshToken;
+        }
+
+        public void Refresh(RefreshToken token)
+        {
+            token.ValidTo = DateTime.UtcNow.AddHours(24);
+            token.TotalRefresh++;
+        }
+
+        public void Revoke(RefreshToken token)
+        {
+            token.IsRevoked = true;
+        }
+    }
+}
