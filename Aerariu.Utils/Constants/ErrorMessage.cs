6f7d06d6ed582b70cdf74a37bcf3fc5395dc320d
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-17 02:24:59 +0800

    Add ErrorMessage and ResponseMessage constants.

---
+++ Aerariu.Utils/Constants/ErrorMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerariu.Utils.Constants
+{
+    public class ErrorMessage
+    {
+        public const string DuplicateUsername = "Username is already used.";
+        public const string InvalidCredentials = "Invalid username or password.";
+    }
+}
---
+++ Aerariu.Utils/Constants/ResponseMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerariu.Utils.Constants
+{
+    public class ResponseMessage
+    {
+        public const string RegistrationSuccess = "Registered successfully.";
+    }
+}
