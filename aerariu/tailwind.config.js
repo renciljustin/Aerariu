595094de488374bec2c093e9a47d34acd91f68af
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-18 22:46:26 +0800

    Add initial packages.
    Install @reduxjs/react, react-redux, tailwind, postcss. and autoprefixer.

---
+++ aerariu/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  darkMode: 'class', // 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
