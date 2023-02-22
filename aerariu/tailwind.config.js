cb9577f97abb626d19b98204cf6532bf9d090035
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-23 01:41:33 +0800

    Add typography.
    Set Fredoka as font headings and Inter as font body.

--- aerariu/tailwind.config.js
+++ aerariu/tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: 'class', // 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ['Fredoka', ...defaultTheme.fontFamily.sans],
+      body: ['Inter', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   variants: {
