3d8dba11cc406aeac78afafbe4ad157aaef0a477
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-24 04:54:25 +0800

    Update Container component.
    Update padding on different screen sizes.

--- aerariu/components/layout/Container.tsx
+++ aerariu/components/layout/Container.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Container = ({ children }: React.PropsWithChildren) => {
-  return <div className="max-w-screen-xl mx-auto">{children}</div>;
+  return (
+    <div className="max-w-screen-xl mx-auto px-2 md:px-4 xl:px-0">
+      {children}
+    </div>
+  );
 };
 
 export default Container;
