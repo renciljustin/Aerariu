f80775d760f20ad04e8abea878bc6b511d82e5c5
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-24 04:56:37 +0800

    Update global styling.
    Change the text color to text-slate-700.
    Code clean-up on global css file.

--- aerariu/pages/_document.tsx
+++ aerariu/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-pink-100">
+      <body className="text-slate-700 bg-pink-100">
         <Main />
         <NextScript />
       </body>
