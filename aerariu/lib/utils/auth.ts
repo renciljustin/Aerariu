3c705a9b5ecb00476ba182306a64088cc6a0eb76
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-26 01:33:58 +0800

    Add auth utils.

--- aerariu/lib/utils/auth.ts
+++ aerariu/lib/utils/auth.ts
@@ -2,3 +2,15 @@ import { isDevelopment } from './common';
 
 export const getApiEndpoint = () =>
   isDevelopment() ? 'http://localhost:5038/api' : 'https://aerariu.com/api';
+
+export const getAuthEndpoint = () => [getApiEndpoint(), 'Auth'].join('/');
+
+export const getAccessToken = () =>
+  typeof localStorage !== 'undefined'
+    ? localStorage.getItem('access-token')
+    : null;
+
+export const removeAccessToken = () => localStorage.removeItem('access-token');
+
+export const setAccessToken = (value: string) =>
+  localStorage.setItem('access-token', value);
