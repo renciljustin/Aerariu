2d32f5d24da2011c1dff27a1e8faa9017cabdb5a
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-28 01:49:17 +0800

    Append ResponseMessage constant.

--- Aerariu.Utils/Constants/ResponseMessage.cs
+++ Aerariu.Utils/Constants/ResponseMessage.cs
@@ -2,6 +2,9 @@
 {
     public class ResponseMessage
     {
+        //Auth
+        public const string GeneratedNewToken = "Generated a new token.";
         public const string RegistrationSuccess = "Registered successfully.";
+        public static string LoginSuccess(string user) => $"Logged in with user {user}.";
     }
 }
