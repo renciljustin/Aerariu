a3c1e4c191eafa980193a87312a7ec6c4fa7debd
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-02-28 01:48:24 +0800

    Add AuthorizationToken model.

---
+++ Aerariu.API/Lib/Middleware/AuthorizationToken.cs
@@ -0,0 +1,8 @@
+﻿namespace Aerariu.API.Lib.Middleware
+{
+    public class AuthorizationToken
+    {
+        public required string AccessToken { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
