4a610c015fdb692df205bebcdbcaf5140f815e19
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-03-03 05:02:16 +0800

    Add CustomError type.

--- aerariu/lib/utils/common-types.ts
+++ aerariu/lib/utils/common-types.ts
@@ -1,5 +1,13 @@
+//common
 export type Nullable<T> = T | null | undefined;
 
+export type ResponseData<T> = {
+  message: string;
+  resultData: T;
+  statusCode: number;
+};
+
+//redux state
 export type StateStatus = {
   loading: boolean;
   success: boolean;
@@ -9,4 +17,18 @@ export type StateStatus = {
 export type StateWithStatus<T> = {
   data: Nullable<T>;
   status: StateStatus;
+};
+
+//exceptions
+
+export type CustomErrorResponse = {
+  data: string | ResponseData<string>;
+  status: string;
+  statusText: Nullable<string>;
+};
+
+export type CustomError = {
+  code: string;
+  message: string;
+  response: CustomErrorResponse;
 };
