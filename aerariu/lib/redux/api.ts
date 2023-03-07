6550ea1935c701fe7c74dfd90a36e48c7cca2547
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-03-08 01:42:36 +0800

    Add authorization in http headers.

--- aerariu/lib/redux/api.ts
+++ aerariu/lib/redux/api.ts
@@ -1,10 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { HYDRATE } from 'next-redux-wrapper';
-import { getApiEndpoint } from '../utils/auth';
+import { getAccessToken, getApiEndpoint } from '../utils/auth';
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: getApiEndpoint(),
+    prepareHeaders: (headers, { getState }) => {
+      const token = getAccessToken();
+
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+        return headers;
+      }
+    },
   }),
   extractRehydrationInfo(action, { reducerPath }) {
     if (action.type === HYDRATE) {
